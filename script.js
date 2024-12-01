fc5eb25b721cb17b2c68c6b3c08bfdafe57a6326
Yasahiru <legendem718@gmail.com>
2024-12-01 23:51:05 +0100

    Second Commit

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -16,9 +24,10 @@ window.onscroll = ()=>{
             });
         }
     });
-}
-
-window.onscroll = ()=>{
+    
     let header = document.querySelector('header');
     header.classList.toggle('sticky',window.scrollY > 100)
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
